8778100e57649fa937f41a0410aa3731ef3d41fa
Javier de Arcos <javier.dearcos@liferay.com>
2021-12-02 06:49:17 +0100

    Add more test to be sure our solution is robust

--- src/test/java/es/javierdearcos/adventofcode2021/day1/SonarTest.java
+++ src/test/java/es/javierdearcos/adventofcode2021/day1/SonarTest.java
@@ -2,6 +2,7 @@ package es.javierdearcos.adventofcode2021.day1;
 
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -19,4 +20,30 @@ public class SonarTest {
         assertEquals(7, result);
     }
 
+    @Test
+    public void testSweepWithEmptyList() {
+        List<Integer> emptyMeasureList = Collections.emptyList();
+
+        int result = sonar.sweep(emptyMeasureList);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testSweepWithAllMeasuresIncreasing() {
+        List<Integer> increasingMeasureList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        int result = sonar.sweep(increasingMeasureList);
+
+        assertEquals(increasingMeasureList.size() - 1, result);
+    }
+
+    @Test
+    public void testSweepWithAllMeasuresDecreasing() {
+        List<Integer> decreasingMeasureList = List.of(10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+
+        int result = sonar.sweep(decreasingMeasureList);
+
+        assertEquals(0, result);
+    }
 }
